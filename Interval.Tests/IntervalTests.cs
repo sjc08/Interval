aa4e362d2bb6c971a3fe63bfb7eb1d88f35eff28
AlexSJC <cn_sjc@outlook.com>
2025-03-05 23:51:48 +0800

    feat: update

---
+++ Interval.Tests/IntervalTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Asjc.Interval.Tests
+{
+    [TestClass]
+    public class Interval
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(new Interval<int>(0, 1).Equals(new Interval<int>(0, 1)));
+            Assert.IsTrue(new Interval<int>(0, 1).Equals((object)new Interval<int>(0, 1)));
+            Assert.IsTrue(new Interval<int>(0, 1) == new Interval<int>(0, 1));
+        }
+    }
+}
---
+++ Interval/Interval.cs
@@ -0,0 +1,41 @@
+﻿#pragma warning disable CS1591
+namespace Asjc.Interval
+{
+    public class Interval<T> : IEquatable<Interval<T>> where T : struct, IComparable
+    {
+        public Interval() { }
+
+        public Interval(T? start, T? end)
+        {
+            if (start.HasValue && end.HasValue && start.Value.CompareTo(end.Value) > 0)
+                throw new ArgumentException($"{nameof(start)} must be less than or equal to {nameof(end)}");
+            Start = start;
+            End = end;
+        }
+
+        public T? Start { get; }
+
+        public T? End { get; }
+
+        public bool Equals(Interval<T>? other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Nullable.Equals(Start, other.Start) &&
+                   Nullable.Equals(End, other.End);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj is Interval<T> other && Equals(other);
+        }
+
+        public override int GetHashCode() => (Start, End).GetHashCode();
+
+        public static bool operator ==(Interval<T> left, Interval<T> right) => Equals(left, right);
+
+        public static bool operator !=(Interval<T> left, Interval<T> right) => !Equals(left, right);
+    }
+}
