d935253eeec74f5ab09500853c321133d0518d62
AlexSJC <cn_sjc@outlook.com>
2025-03-07 00:16:21 +0800

    feat: add interface

---
+++ Interval/IInterval.cs
@@ -0,0 +1,19 @@
+﻿namespace Asjc.Interval
+{
+    /// <summary>
+    /// Represents a generic interval with start and end boundaries.
+    /// </summary>
+    /// <typeparam name="T">The type of the interval boundaries.</typeparam>
+    public interface IInterval<T> where T : struct, IComparable
+    {
+        /// <summary>
+        /// Gets the start boundary of the interval.
+        /// </summary>
+        T? Start { get; }
+
+        /// <summary>
+        /// Gets the end boundary of the interval.
+        /// </summary>
+        T? End { get; }
+    }
+}
--- Interval/Interval.cs
+++ Interval/Interval.cs
@@ -1,10 +1,19 @@
-﻿#pragma warning disable CS1591
-namespace Asjc.Interval
+﻿namespace Asjc.Interval
 {
-    public class Interval<T> : IEquatable<Interval<T>> where T : struct, IComparable
+    /// <inheritdoc cref="IInterval{T}"/>
+    public class Interval<T> : IInterval<T>, IEquatable<Interval<T>> where T : struct, IComparable
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Interval{T}"/> class with null boundaries.
+        /// </summary>
         public Interval() { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Interval{T}"/> class with the specified boundaries.
+        /// </summary>
+        /// <param name="start">The start boundary of the interval.</param>
+        /// <param name="end">The end boundary of the interval.</param>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="start"/> is greater than <paramref name="end"/>.</exception>
         public Interval(T? start, T? end)
         {
             if (start.HasValue && end.HasValue && start.Value.CompareTo(end.Value) > 0)
@@ -13,10 +22,13 @@ namespace Asjc.Interval
             End = end;
         }
 
+        /// <inheritdoc/>
         public T? Start { get; }
 
+        /// <inheritdoc/>
         public T? End { get; }
 
+        /// <inheritdoc/>
         public bool Equals(Interval<T>? other)
         {
             if (other is null) return false;
@@ -25,6 +37,7 @@ namespace Asjc.Interval
                    Nullable.Equals(End, other.End);
         }
 
+        /// <inheritdoc/>
         public override bool Equals(object? obj)
         {
             if (obj is null) return false;
@@ -32,6 +45,7 @@ namespace Asjc.Interval
             return obj is Interval<T> other && Equals(other);
         }
 
+        /// <inheritdoc/>
         public override int GetHashCode() => (Start, End).GetHashCode();
 
         public static bool operator ==(Interval<T> left, Interval<T> right) => Equals(left, right);
